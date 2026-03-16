81af040426b1ae12854a935dc1b34d0621690896
rockerwei <rocker.wei.watsons@gmail.com>
2026-03-16 14:15:47 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,36 @@
-const CACHE_NAME = "kids-game-cache-v1";
+const CACHE_NAME = "kids-game-cache-v2";
+
 const urlsToCache = [
   "./",
-  "./index.html"
+  "./index.html",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
   );
 });
